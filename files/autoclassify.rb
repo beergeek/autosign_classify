d147f472d18c763e25c025bcc76cd1c6839bccee
beergeek <brett.gray@puppetlabs.com>
2014-10-19 22:31:32 +1100

    Updates to scripts for classifying and signer

--- files/autoclassify.rb
+++ files/autoclassify.rb
@@ -2,32 +2,41 @@
 
 require 'puppet'
 require 'puppet/ssl/certificate'
+require 'syslog'
 
-def get_clientcert_name(path)
-  File.basename(path, 'pem')
-end
+# lets setup some logging
+Syslog.open("autoclassifier", Syslog::LOG_PID, Syslog::LOG_SYSLOG)
 
 def get_and_write(clientcert)
-  puts clientcert
+  # is it really a file?
   if File.exists?(clientcert)
-    cert = Puppet::SSL::Certificate.new(get_clientcert_name(clientcert))
-    cert_file = cert.read(clientcert)
-    extensions = cert_file.extensions
-    extensions.each do |line|
-      if line.oid == '1.3.6.1.4.1.34380.1.2.2'
-        classifier_data = line.value
-        system("/opt/puppet/bin/rake -f /opt/puppet/share/puppet-dashboard/Rakefile RAILS_ENV=production node:add[#{clientcert},'default',#{classifier_data},skip]")
+    begin
+      # This will fail if it is not a certificate, hence the rescue below
+      cert = Puppet::SSL::Certificate.new(File.basename(clientcert, 'pem'))
+      cert_file = cert.read(clientcert)
+      cert_file.extensions.each do |line|
+        # Does it have the extension we need?
+        if line.oid == '1.3.6.1.4.1.34380.1.2.2'
+          classifier_data = line.value
+          # Call the Rake API to classify.
+          # This will not work if the node already exists
+          Syslog.log(Syslog::LOG_INFO, "Attempting to classify #{File.basename(clientcert, 'pem')}")
+          system("/opt/puppet/bin/rake -f /opt/puppet/share/puppet-dashboard/Rakefile RAILS_ENV=production node:add[#{clientcert},'default',#{classifier_data},skip]")
+        end
       end
+    rescue Exception => e
+      Syslog.log(Syslog::LOG_WARNING, "We had an error with the autoclassifier: #{e}")
     end
   else
-    puts "Oops"
+    Syslog.log(Syslog::LOG_INFO, "#{File.basename(clientcert)} is not a certificate")
   end
 end
 
 if File.file?(ARGV[0])
   get_and_write(ARGV[0])
 elsif File.directory?(ARGV[0])
-  Dir.ARGV[0].each do |single_file|
-    get_and_write(single_file)
+  Dir.foreach(ARGV[0]) do |single_file|
+    next if single_file == '.' or single_file == '..'
+    get_and_write(File.join(ARGV[0],single_file))
   end
 end
